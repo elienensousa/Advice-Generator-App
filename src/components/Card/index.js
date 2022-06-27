1ddb4d624d2945a1bc1e1807e1f280aa514cd1dc
Eliene Sousa <eliene.nunes1@hotmail.com>
2022-06-27 01:02:10 -0300

    Consumindo Advice Slip API

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,22 +1,48 @@
 import './styles.scss'
 import dice from '../../assets/icon-dice.svg'
+import { useEffect, useState } from 'react';
+
+
 
 export function Card(){
+    const [advice, setAdvice] = useState({id:0, advice: ""});
+
+    
+    useEffect(() => {
+       handleClick()
+    },[])
+
+   async function handleClick(){
+        await fetch('https://api.adviceslip.com/advice')
+        .then(response =>{
+            if(response.ok){
+                return response.json()
+            }
+            throw response
+        }).then(data =>{
+            setAdvice(data.slip)
+        })
+        .catch(error =>{
+            console.error("Error: ", error)
+        })
+
+    }
+
     return (
     <>
         <main>
             <div>
                 <p className='title'>
-                    ADVICE #201
+                    ADVICE #{advice.id}
                 </p>
 
                 <p className='advice'>
-                    "Don't burn bridges"
+                    "{advice.advice}"
                 </p>
 
-                <div       className='divider'></div>
+                <div className='divider'></div>
 
-                <button>
+                <button onClick={handleClick}>
                     <img  src={dice} alt='Another' />
                 </button>
             </div>
