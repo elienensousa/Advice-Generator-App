997fc5061be507d3d1b628f258a17b8e26aa5332
Eliene Sousa <eliene.nunes1@hotmail.com>
2022-06-27 01:22:04 -0300

    Component Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Card } from "./components/Card";
+import { Footer } from "./components/Footer";
 import './styles/styles.scss'
 
 function App() {
   return (
+    <>
     <Card />
+    <Footer />
+    </>
   );
 }
 
